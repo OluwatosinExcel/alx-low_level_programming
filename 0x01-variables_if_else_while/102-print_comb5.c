56f1fa66144c7f042d5c467f4d3e444473b20f31
uniquexcel <unniquexcelf@gmail.com>
2022-04-08 16:13:10 -0700

    My commit for task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -16,15 +16,15 @@ for (num2 = num1 + 1; num2 <= 99; num2++)
 {
 putchar((num1 / 10) + '0');
 putchar((num1 % 10) + '0');
-putchar(' ');
+putchar( );
 putchar((num2 / 10) + ‘0’);
 putchar((num2 % 10) + ‘0’);
 
 if (num1 == 98 && num2 == 99)
-continue
+continue;
 
 putchar(‘,’);
-putchar( );
+putchar(‘ ‘);
 }
 }
 
