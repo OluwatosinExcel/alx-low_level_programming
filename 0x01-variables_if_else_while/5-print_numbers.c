7afc996c90192c548c50e8a4088528beb693ff4c
uniquexcel <unniquexcelf@gmail.com>
2022-04-12 07:41:41 -0700

    My commit tp print _putchar

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -11,10 +11,10 @@ int main(void)
 int i;
 for (i = 0; i < 10; ++i)
 {
-printf(“%i”, i);
+printf(‘%i’, i);
 }
 
-putchar(“\n”);
+putchar(‘\n’);
 
 return (0);
 
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Low level language readme
+My readme for 0x02-functions_nested_loops
