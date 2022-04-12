1fc5ab8d803fcf61b820a4f56da169740341ea46
uniquexcel <unniquexcelf@gmail.com>
2022-04-11 19:24:46 -0700

    My commit for task 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
+
 /**
- * main - Entry point
+ * main - entry point
  *
- * Return: Always 0 (Success/correct)
+ * Description: Print numbers 0 to 9 on one Line
+ * Return: 0
  */
 int main(void)
-
 {
-int i;
+char i;
+
+for (i = ‘0’; i <= ‘9’; ++i)
+putchar(i);
+}
+
+putchar('\n');
+
+return (0);
+}
