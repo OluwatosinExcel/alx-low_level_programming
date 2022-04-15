6702395c5aa4fd869e1b3338d8a9fa31158fa26d
uniquexcel <unniquexcelf@gmail.com>
2022-04-15 09:18:35 -0700

    My commit for task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,18 +1,19 @@
 #include "main.h"
+
 /**
  * print_numbers - print numbers
  * Return: Always 0.
  */
 void print_numbers(void)
 {
-int i;
+  int i;
 
-i = 0;
-while (i < 10)
-{
-_putchar(i + '0');
-i++;
-}
+  i = 0;
+  while (i < 10)
+  {
+  _putchar(i + '0');
+  i++;
+  }
 
-_putchar('\n');
+  _putchar('\n');
 }
