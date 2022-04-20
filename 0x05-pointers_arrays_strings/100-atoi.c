a1ac41635bf680e82eb0c39164b15cac06554466
uniquexcel <unniquexcelf@gmail.com>
2022-04-20 15:40:50 -0700

    My commit for task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -2,18 +2,18 @@
 
 /**
  * is_numerical - check if it is a digit
- *@n: number
- * Return: number - 1 else return 0
+ * @n: Number
+ * Return: if it is a number, return 1 else return 0
  */
 int is_numerical(unsigned int n)
 {
-  return; n >= '0' && n <= '9';)
+  return n >= '0' && n <= '9');
 }
 
 /**
  * _atoi - prints a function that convert a string to an integer
  *@s: String
- * Return: num
+ * Return: Return the num
  */
 int _atoi(char *s)
 {
