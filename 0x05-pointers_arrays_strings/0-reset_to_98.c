1947d199621d6cc02fa0ff20f5bd8a636b3d9528
uniquexcel <unniquexcelf@gmail.com>
2022-04-20 03:24:00 -0700

    Commit to write a function that takes a pointer to an int as parameter

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - A function that takes a pointer to an int as parameter
+ *
+ * Return: Always 0.
+ */
+void reset_to_98(int *n);
+{
+
+	int n;
+
+	n = 402;
+	printf("n=%d\n", n)
+	reset_to_98(&n);
+	printf("n=%d\n", n);
+	return (0);
+}
