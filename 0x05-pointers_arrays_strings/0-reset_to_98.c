d3c0b8e59495be1dca657faba3483cf02d7f44fd
uniquexcel <unniquexcelf@gmail.com>
2022-04-20 03:34:17 -0700

    My commit for task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,13 +1,13 @@
 #include "main.h"
 #include <stdio.h>
 
+void reset_to_98(int *n);
 /**
  * main - A function that takes a pointer to an int as parameter
  *
- * Return: Always 0.
+ * Return: Always 0
  */
-void reset_to_98(int *n);
-
+int main(void)
 {
 	int n;
 
