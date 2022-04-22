a1653ee57ced5bfa74993c4122d745ae35409dc2
uniquexcel <unniquexcelf@gmail.com>
2022-04-22 06:57:34 -0700

    My commit for task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,13 +2,14 @@
 #include <stdio.h>
 
 /**
- * strcat - A function that concatenates two strings
- *@dest - A function that will be updated
- *@src - A function that will be updated / changed
+ * _strcat - A function that concatenates two strings
+ *@dest: A function that will be updated
+ *@src: A function that will be updated / changed
  * Return: char - correct answer
  */
 
 char *_strcat(char *dest, char *src)
 {
-strcat(*src, *dest);
+strcat(*dest, *src);
+_putchar("strcat( *dest, *src): %s\n", *dest);
 }
