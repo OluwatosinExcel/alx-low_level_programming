2ae9ab6ed205df358e59b734503219e3629ee338
uniquexcel <unniquexcelf@gmail.com>
2022-04-22 08:26:08 -0700

    My commit for task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,18 +1,31 @@
 #include "main.h"
-#include <stdio.h>
-#include <string.h>
 /**
  *_strcat - A function that concatenates two strings
- *@dest: A function that will be updated
- *@src: A function that will be updated / changed
- * Return: char - correct answer
+ *@dest: A pointer to a function that will be updated
+ *@src: A pointer to a function that will be updated / changed
+ * Return: dest
  */
 
 char *_strcat(char *dest, char *src)
 
 {
-char dest = "Tosin";
-char src = "Excel";
+  int m;
+  int n;
+  
+  m = 0;
+  while (dest[m] != '\0')
+    {
+      m++;
+    }
 
-strcat(dest, src);
+  n = 0;
+  while (src[n] != '\0')
+    {
+      dest[m] = src[n];
+      n++;
+      m++;
+    }
+  dest[m] = '\0';
+
+return (dest);
 }
