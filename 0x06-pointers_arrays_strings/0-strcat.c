24d3e76c636dac2f61b5fdd3b270ec85fd64cda8
uniquexcel <unniquexcelf@gmail.com>
2022-04-23 13:15:53 -0700

    Myy commit for task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,23 +9,23 @@
 char *_strcat(char *dest, char *src)
 
 {
-  int m;
-  int n;
-  
-  m = 0;
-  while (dest[m] != '\0')
-    {
-      m++;
-    }
+int m;
+int n;
 
-  n = 0;
-  while (src[n] != '\0')
-    {
-      dest[m] = src[n];
-      n++;
-      m++;
-    }
-  dest[m] = '\0';
+m = 0;
+while (dest[m] != '\0')
+{
+m++;
+}
+
+n = 0;
+while (src[n] != '\0')
+{
+dest[m] = src[n];
+n++;
+m++;
+}
+dest[m] = '\0';
 
 return (dest);
 }
