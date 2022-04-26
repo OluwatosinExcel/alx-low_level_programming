272efe70be6723665eac47f7646c689638d6ffcd
uniquexcel <unniquexcelf@gmail.com>
2022-04-26 10:07:57 -0700

    My commit for task 0

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- *_memcpy - A function that copies memory area.
+ *_memcpy - A function that copies memory area
  *@dest: destination
  *@src: source
  *@n: bytes from memory area
@@ -9,7 +9,7 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 unsigned int t;
- 
+
 for (t = 0; t < n; t++)
 *(dest + t) = *(src + t);
 
